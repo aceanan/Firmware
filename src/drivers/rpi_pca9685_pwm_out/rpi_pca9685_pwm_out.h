de11dfac10eb29dd35ae7a07b546a54bda10a32b
crossa <421395590@qq.com>
2017-07-03 22:05:48 +0800

    Fix error

--- src/drivers/rpi_pca9685_pwm_out/rpi_pca9685_pwm_out.h
+++ src/drivers/rpi_pca9685_pwm_out/rpi_pca9685_pwm_out.h
@@ -95,7 +95,7 @@ int32_t _pwm_max;
 //PCA9685 device
 ::PCA9685 pwm;
 #if defined(__DF_ARM_GENERIC)
-int _pca9685_bus=__DF_PCA9685_BUS
+int _pca9685_bus=__DF_PCA9685_BUS;
 #else
 int _pca9685_bus=1; //默认兼容树莓派
 #endif
